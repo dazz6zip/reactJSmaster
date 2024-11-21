61d46320daa7bd4d84738397097c7b20b8c6b6b0
dazz6 <dazz6@kakao.com>
2024-11-21 16:59:47 +0900

    6.6 Forms

--- react-masterclass-02/src/ToDoList.tsx
+++ react-masterclass-02/src/ToDoList.tsx
@@ -1,26 +1,56 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+//   const [toDoError, setToDoError] = useState("");
+
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDoError("");
+//     setToDo(value);
+//   };
+
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (toDo.length < 10) {
+//       return setToDoError("To do should be longer");
+//     }
+//     console.log("submit");
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           onChange={onChange}
+//           value={toDo}
+//           placeholder="Write a to do"
+//         ></input>
+//         <button>Add</button>
+//         {toDoError !== "" ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  // watch : form 입력값들의 변화를 관찰할 수 있게 해 주는 함수
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="Write a to do"
-        ></input>
-        <button>d</button>
+      <form>
+        <input {...register("email")} placeholder="Email"></input>
+        <input {...register("firstName")} placeholder="First Name"></input>
+        <input {...register("lastName")} placeholder="Last Name"></input>
+        <input {...register("username")} placeholder="Username"></input>
+        <input {...register("password")} placeholder="Password"></input>
+        <input {...register("password1")} placeholder="Password1"></input>
+        {/* register("toDo")의 반환값을 분해하여 properties로 */}
+        <button>Add</button>
       </form>
     </div>
   );
