350d065551826752c018219eebed01e5af133f64
dazz6 <dazz6@kakao.com>
2024-11-22 16:15:22 +0900

    6.7 Form Validation

--- react-masterclass-02/src/ToDoList.tsx
+++ react-masterclass-02/src/ToDoList.tsx
@@ -37,18 +37,50 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit, formState } = useForm();
   // watch : form 입력값들의 변화를 관찰할 수 있게 해 주는 함수
-  console.log(watch());
+  // handleSubmit : validation
+  // formState : error 등의 상태 저장
+
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+
+  console.log(formState.errors);
+
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Email"></input>
-        <input {...register("firstName")} placeholder="First Name"></input>
-        <input {...register("lastName")} placeholder="Last Name"></input>
-        <input {...register("username")} placeholder="Username"></input>
-        <input {...register("password")} placeholder="Password"></input>
-        <input {...register("password1")} placeholder="Password1"></input>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        {/* handleSubmit(유효할 때 호출되는 함수(필수), 유효하지 않을 때 호출되는 함수(선택)) 
+        submit event가 발생되면 handleSubmit이 해야 하는 모든 validation을 마친 후 함수를 호출함*/}
+        <input
+          {...register("email", { required: true, minLength: 10 })}
+          placeholder="Email"
+        ></input>
+        {/* input 태그에서 지원하는 required 보다 안전 (html 태그는 브라우저에서 수정하기 너무도 쉽기 때문에) */}
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        ></input>
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        ></input>
+        <input
+          {...register("username", { required: true })}
+          placeholder="Username"
+        ></input>
+        <input
+          {...register("password", { required: true })}
+          placeholder="Password"
+        ></input>
+        <input
+          {...register("password1", { required: "Password is required" })}
+          placeholder="Password1"
+        ></input>
         {/* register("toDo")의 반환값을 분해하여 properties로 */}
         <button>Add</button>
       </form>
