d74f3790357e568f69cf7e087d7d779d44eb7776
dazz6 <dazz6@kakao.com>
2024-11-23 01:56:05 +0900

    6.14 Immutability Part One

--- react-masterclass-02/src/components/ToDo.tsx
+++ react-masterclass-02/src/components/ToDo.tsx
@@ -1,11 +1,29 @@
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "./atoms";
+import { useEffect } from "react";
 
 function ToDo({ text, category, id }: IToDo) {
+  // ToDoList-final.tsx 에서 넘겨 준 text, category, id
+
   const setToDos = useSetRecoilState(toDoState);
 
   const onClick = (newCategory: IToDo["category"]) => {
-    console.log("newCategory : " + newCategory);
+    // 상태 button을 클릭하면 어떤 버튼을 클릭했는지 인자로 넘어옴
+    // console.log("newCategory : " + newCategory);
+
+    // 현재 atom(recoil)에 저장되어 있는 ToDos의 값을 변경
+    setToDos((oldToDos) => {
+      // category 변경할 todo의 id 찾기
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      //   console.log(targetIndex);
+
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: newCategory };
+
+      // 현재 저장되어 있는 모든 ToDos를 순회하여 ToDoList-final.tsx에서 받은 id와 같은 자료 찾기
+
+      return oldToDos;
+    });
   };
 
   // 인자로 넘기지 않고 button 태그에 name을 부여해서 event.currentTarget.name으로 구분하는 방법도 있음
