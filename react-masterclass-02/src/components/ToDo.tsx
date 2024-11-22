4eff547091040558bb5a49af549b30cc4433e4eb
dazz6 <dazz6@kakao.com>
2024-11-23 01:21:49 +0900

    6.13 Categories

--- react-masterclass-02/src/components/ToDo.tsx
+++ react-masterclass-02/src/components/ToDo.tsx
@@ -1,13 +1,28 @@
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "./atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (newCategory: IToDo["category"]) => {
+    console.log("newCategory : " + newCategory);
+  };
+
+  // 인자로 넘기지 않고 button 태그에 name을 부여해서 event.currentTarget.name으로 구분하는 방법도 있음
+  // const { currentTarget : {name}, } = event
+  // 이 경우 event의 type은 React.MouseEvent<HTMButtonElement>
   return (
     <li>
-      <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      <span>{text}</span>&emsp;
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>DOING</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>TO DO</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>DONE</button>
+      )}
     </li>
   );
 }
