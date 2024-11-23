036b6534e532a45121a2235ed97a06c7181ccdbf
dazz6 <dazz6@kakao.com>
2024-11-23 11:09:40 +0900

    6.16 Selectors part One

--- react-masterclass-02/src/components/ToDoList-final.tsx
+++ react-masterclass-02/src/components/ToDoList-final.tsx
@@ -1,11 +1,11 @@
 import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
-import { toDoState } from "./atoms";
+import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { toDoSelector, toDoState } from "./atoms";
 import CreateToDo, { IForm } from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const [toDos] = useRecoilState(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   // 일반 useState 처럼 useRecoilState를 쓸 수 있음 (값과 setter 반환)
   // useRecoilState = useRecoilValue + useSetRecoilState
   // 여기서 인자로 들어가는 toDoState 는 위에서 선언한 toDoState atom을 가져오거나, 수정하겠다는 의미
@@ -15,8 +15,21 @@ function ToDoList() {
       <h1>투두리스트</h1>
       <hr />
       <CreateToDo></CreateToDo>
+      <h2>TODO</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo}></ToDo>
+        ))}
+      </ul>
+      <h2>DOING</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo}></ToDo>
+        ))}
+      </ul>
+      <h2>DONE</h2>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo}></ToDo>
         ))}
       </ul>
--- react-masterclass-02/src/components/atoms.tsx
+++ react-masterclass-02/src/components/atoms.tsx
@@ -1,4 +1,5 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+// selector : atom의 output을 변형시키는 도구
 
 export interface IToDo {
   text: string;
@@ -9,6 +10,20 @@ export interface IToDo {
 // 전역으로 쓸 atom 선언 (key : toDo, 기본값 : 빈 배열)
 export const toDoState = atom<IToDo[]>({
   // IToDo 타입으로 이루어진 배열이 들어올 수 있다는 뜻
-  key: "toDo", // atom을 구분하기 위한 unique key
+  key: "toDoState", // atom을 구분하기 위한 unique key
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      // toDos 데이터를 category별로 나눈 배열 return
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+    // return 값이 selector의 value
+  },
 });
