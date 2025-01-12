7c32c0904a37f087fece0d9fec67abb0bab897d5
dazz6 <dazz6@kakao.com>
2025-01-12 17:28:47 +0900

    7.1 Set Selectors

--- react-masterclass-03/src/App.tsx
+++ react-masterclass-03/src/App.tsx
@@ -6,8 +6,13 @@ function App() {
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
   };
+  const [hours, setHours] = useRecoilState(hourSelector);
+  // 첫 번째 인자 : selector의 get, 두 번째 인자 : selector의 set
 
-  const hours = useRecoilValue(hourSelector);
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+    // 입력값(string)을 정수(int)로 변환하기 위한 + 삽입
+  };
 
   return (
     <div>
@@ -18,7 +23,12 @@ function App() {
         placeholder="Minutes"
       />
       분 <br />
-      <input type="number" placeholder="Hours" value={hours} />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
       시간
     </div>
   );
--- react-masterclass-03/src/atoms.tsx
+++ react-masterclass-03/src/atoms.tsx
@@ -4,11 +4,16 @@ export const minuteState = atom({
   key: "minutes",
   default: 0,
 });
-export const hourSelector = selector({
+
+export const hourSelector = selector<number>({
   key: "hours",
   get: ({ get }) => {
     const minutes = get(minuteState);
     // selector 내에서 get -> atom 값 가져오기
     return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
